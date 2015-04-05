01f6af6f5464fa328641a905aacdd2074f0c91bf
Javier Lopez <m@javier.io>
2015-04-05 04:50:28 -0500

    add git path route

--- env-musl-morpheus.sh
+++ env-musl-morpheus.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #cp to /etc/build in a bifrost env
 #https://github.com/minos-org/bifrost-build/blob/master/util/env-hooks.sh
-export PATH=$PATH:/opt/musl/bin:/opt/i486-linux-musl/bin
+export PATH=$PATH:/opt/git/bin:/opt/musl/bin:/opt/i486-linux-musl/bin
